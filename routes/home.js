8e6845d88ea88160a67534c01ca88db7f19870dd
nilekazr <nile.ausar1@gmail>
2020-11-23 17:40:21 -0500

    sharing is caring

---
+++ routes/home.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const db = require('../models');
+const router = express.Router();
+const axios = require('axios');
+const layouts = require('express-ejs-layouts');
+
